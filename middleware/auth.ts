c04589cb13a73193952b8a7fd721a9c963b286c3
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-03-10 22:58:19 +0900

    add email tracker

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,3 +1,4 @@
+import { event } from "vue-gtag";
 import { useAuthStore } from "~/store/auth";
 
 export let interval: NodeJS.Timer | null = null;
@@ -13,4 +14,11 @@ async function run() {
   const authStore = useAuthStore();
   const res = await getAccessToken();
   authStore.setAccessToken(res.accessToken);
+  getUserInfo().then((res) => {
+    event("login", {
+      event_category: "auth",
+      event_label: "google",
+      value: res.email,
+    });
+  });
 }
